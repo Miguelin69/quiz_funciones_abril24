3fa127a2d6ba320f9553423de8601e238395a4f5
Miguelin69 <miguel2234505@correo.uis.edu.co>
2023-04-24 08:27:44 -0500

    se agrego programa

---
+++ er.py
@@ -0,0 +1,39 @@
+# ejercicio 1
+
+def multiplicacion(n):
+    mul1 = 1 * n
+    
+    return mul1 
+
+p = 1
+for i in range(1):
+    N = int(input("ingrsa el numero: "))
+    resultado1 = multiplicacion(N)
+    resultado2 = multiplicacion(N) * 2
+    resultado3 = multiplicacion(N) * 3
+    resultado4 = multiplicacion(N) * 4
+    resultado5 = multiplicacion(N) * 5
+    resultado6 = multiplicacion(N) * 6
+    resultado7 = multiplicacion(N) * 7
+    resultado8 = multiplicacion(N) * 8
+    resultado9 = multiplicacion(N) * 9
+    resultado10 = multiplicacion(N) * 10
+
+
+    print("--------------------------------")
+    print("      Tabla del  " , str(N))
+    print("--------------------------------")
+    print(f" 1 * {N} = " , str(resultado1) )
+    print(f" 2 * {N} = " , str(resultado2) )
+    print(f" 3 * {N} = " , str(resultado3) )
+    print(f" 4 * {N} = " , str(resultado4) )
+    print(f" 5 * {N} = " , str(resultado5) )
+    print(f" 6 * {N} = " , str(resultado6) )
+    print(f" 7 * {N} = " , str(resultado7) )
+    print(f" 8 * {N} = " , str(resultado8) )
+    print(f" 9 * {N} = " , str(resultado9) )
+    print(f" 10 * {N} = " , str(resultado10) )
+    print("------------------------------")
+    print("------------------------------")
+    print("------------------------------")
+
---
+++ er2.py
@@ -0,0 +1,7 @@
+# ejercicio 2
+
+def ultimo(n):
+    resul = n % 4
+    return resul
+
+p = int(input("Ingrsa el numero: "))
