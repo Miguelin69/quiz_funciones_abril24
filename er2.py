43748f6a4aff3dc4d9cf283e4216391655f4f53c
Miguelin69 <miguel2234505@correo.uis.edu.co>
2023-04-24 08:38:56 -0500

    se agrego programa

--- er2.py
+++ er2.py
@@ -1,7 +1,12 @@
 # ejercicio 2
 
 def ultimo(n):
-    resul = n % 4
+    resul = n % 10
     return resul
 
 p = int(input("Ingrsa el numero: "))
+
+if ultimo(p) == 4:
+    print("El último digito si termina en cuatro ")
+else : 
+    print("El último digito no termina en cuatro ")
